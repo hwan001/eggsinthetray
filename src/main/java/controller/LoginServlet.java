18b6601f6c3600f4be1c7a60af6a7a978433601c
Minwooh <hsoapdc@gmail.com>
2025-05-26 17:35:13 +0900

    [EITT-48] 인가코드 받기

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -1,5 +1,29 @@
 package controller;
 
-public class LoginServlet {
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+    	super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
--- src/main/java/oauth/KakaoOAuthService.java
+++ src/main/java/oauth/KakaoOAuthService.java
@@ -1,5 +1,33 @@
 package oauth;
 
-public class KakaoOAuthService {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+//인가코드요청 
+@WebServlet("/kakaooauth")
+public class KakaoOAuthService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public KakaoOAuthService() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String kakaoUrl = "https://kauth.kakao.com/oauth/authorize"
+				+ "?response_type=code"
+				+ "&client_id=9842f3b5d39972114e3df176dbd060ad"
+				+ "&redirect_uri=http://localhost:8090/eggsinthetray/kakaocallback";
+		response.sendRedirect(kakaoUrl);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
 
 }
+
