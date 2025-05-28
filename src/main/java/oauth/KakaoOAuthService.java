ca6d66166c0599960e17c81258a18e50e13514e9
Minwooh <hsoapdc@gmail.com>
2025-05-28 18:40:03 +0900

    [EITT-63] 로그아웃

--- src/main/java/oauth/KakaoOAuthService.java
+++ src/main/java/oauth/KakaoOAuthService.java
@@ -17,7 +17,7 @@ public class KakaoOAuthService extends HttpServlet {
     }
 
 	
-	
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String clientId = getServletContext().getInitParameter("kakao.client_id");
 		
